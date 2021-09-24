04b272cd18d164413957936e3e078bbc454fe67d
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-25 00:47:16 +0530

    rouuters-added

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const app = express();
+const pinRoute = require('./routes/pins')
 
 dotenv.config()
+
+app.use(express.json)
 
 mongoose.connect(process.env.MONGO_URL, 
     { useNewUrlParser: true, unUnifiedTopology: true }, 
     () => {
     console.log("Connected to MongoDB")
 })
+
+app.use("/api/pins", pinRoute);
 
 app.listen(5000, () => {
     console.log('listening on port 5000...');
