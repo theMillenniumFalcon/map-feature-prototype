1b00bf9d3f4c58c0899766531a62c6c87d51b03d
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-25 03:50:30 +0530

    popup-working

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
+import { Room, Star } from "@material-ui/icons"
+import styled from "styled-components"
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -15,9 +17,72 @@ function App() {
       {...viewport}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+      mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
+    >
+       <Marker latitude={48.858093} longitude={2.294694} offsetLeft={-20} offsetTop={-10}>
+        <Room atyle={{ fontSize: viewport.zoom * 7 }}/>
+       </Marker>
+       <Popup
+          latitude={48.858093}
+          longitude={2.294694}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <Card>
+            <Label>Name</Label>
+              <h3>Eiffell Tower</h3>
+            <Label>Review</Label>
+              <Description>A beautiful place. I like it.</Description>
+            <Label>Rating</Label>
+            <Stars>
+            <Star/>
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+              <Star />
+            </Stars>
+            <Label>Information</Label>
+            <Username>Created by <b>Nishank</b></Username>
+            <Date>1 Hour age</Date>
+          </Card>
+        </Popup>
+    </ReactMapGL>
+    
     </div>
   );
 }
+
+const Card = styled.div`
+width: 250px;
+height: 250px;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+`;
+
+const Label = styled.div`
+width: max-content;
+color: tomato;
+font-size: 13px;
+border-bottom: .5px solid tomato;
+margin: 3px 0;
+`;
+
+const Description = styled.div`
+font-size: 14px;
+`;
+
+const Stars = styled.div`
+color: gold;
+`;
+
+const Username = styled.div`
+font-size: 14px;
+`;
+
+const Date = styled.div`
+font-size: 12px;
+`;
 
 export default App;
